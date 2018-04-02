acaa053a8ea0d9831145ae29f337cd4435cd0644
kieranbs96 <kieranbs96@gmail.com>
2018-04-02 21:19:48 +0100

    Blog

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,30 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/essential', 'airbnb-base'
   ],
   // required to lint *.vue files
   plugins: [
     'vue'
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      js: 'never',
+      vue: 'never'
+    }],
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e' // for e.returnvalue
+      ]
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 }
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,21 +1,62 @@
 <template lang="pug">
   div.wrapper
     app-miniheader
-    .container
+    <div class="blog" :class="{ 'blog--reading': this.post }">
       h1 Blog
+      <blog-feed :filters="filters"/>
+      <blog-post :post="post"/>
+    </div>
     app-footer
 
 </template>
 
 <script>
 import MiniHeader from "../components/MiniHeader";
+import BlogFeed from "../components/BlogFeed";
+import BlogPost from "../components/BlogPost";
 import Footer from "../components/Footer";
 
 export default {
-  name: "Home",
+  name: "blog",
   components: {
-    "app-footer": Footer,
-    "app-miniheader": MiniHeader
+    "app-miniheader": MiniHeader,
+    BlogFeed,
+    BlogPost,
+    "app-footer": Footer
+  },
+  resource: "Blog",
+  props: {
+    post: String,
+    author: String
+  },
+  data() {
+    return {
+      navs: 0,
+      title: "",
+      labels: {
+        post: "",
+        author: ""
+      }
+    };
+  },
+  computed: {
+    content() {
+      return { title: this.title, labels: this.labels };
+    },
+    filters() {
+      let filters = {};
+      if (this.post) filters.post = this.post;
+      if (this.author) filters.author = this.author;
+      return filters;
+    }
+  },
+  watch: {
+    "$route.name"(to, from) {
+      if (to !== from) this.navs++;
+    }
+  },
+  beforeMount() {
+    this.$getResource("blog");
   }
 };
 </script>
